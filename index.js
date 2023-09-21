8ff09c25f028051f0abe8275c4efb25c321822ec
itssvinayak <88770292+itssvinayak@users.noreply.github.com>
2023-09-21 10:56:17 +0530

    bug fixes

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 let key = 'key1';
 let value = 'value1';
 
-// let key = 'kk';
-// let value = 'vv';
 setnx(key, value, (err, res) => {
     console.log(err, res);
     get(key, (err, res) => {
